541ea509a59b03e35bba55d484b46559f1e74756
张洪盛” <1172114711@qq.com>
2025-12-05 20:44:47 +0800

    修改

---
+++ test/HMtest/IFdemo/IFdemo1.java
@@ -0,0 +1,21 @@
+package HMtest.IFdemo;
+
+import java.util.Scanner;
+
+public class IFdemo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入");
+        int a = sc.nextInt();
+        if (a > 2){
+                System.out.println("给出回应");
+        }
+        else
+            {
+                System.out.println("不做出回应");
+            }
+        
+        sc.close();
+    }
+    
+}
